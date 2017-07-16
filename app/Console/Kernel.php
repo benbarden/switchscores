2a42b49c601bc8dfb59dd876d3e59fa939f83498
Ben Barden <github@benbarden.com>
2017-07-16 18:00:15 +0100

    Adding image count to games table; adding command to update it

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Commands\UpdateGameReviewStats::class
+        Commands\UpdateGameReviewStats::class,
+        Commands\UpdateGameImageCount::class,
     ];
 
     /**
