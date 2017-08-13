7e96b9612fc97702b13a60285b7b2ebaec5b53e5
Ben Barden <github@benbarden.com>
2017-08-13 11:53:56 +0100

    Fix: Reordered routes for admin.reviews.link due to 404

--- routes/web.php
+++ routes/web.php
@@ -64,11 +64,11 @@ Route::group(['middleware' => ['auth.admin:admin']], function() {
     Route::post('/admin/reviews/site/add', 'Admin\ReviewSiteController@add')->name('admin.reviews.site.add');
 
     // Reviews: Links
-    Route::get('/admin/reviews/link/{report?}', 'Admin\ReviewLinkController@showList')->name('admin.reviews.link.list');
     Route::get('/admin/reviews/link/add', 'Admin\ReviewLinkController@add')->name('admin.reviews.link.add');
     Route::post('/admin/reviews/link/add', 'Admin\ReviewLinkController@add')->name('admin.reviews.link.add');
     Route::get('/admin/reviews/link/edit/{linkId}', 'Admin\ReviewLinkController@edit')->name('admin.reviews.link.edit');
     Route::post('/admin/reviews/link/edit/{linkId}', 'Admin\ReviewLinkController@edit')->name('admin.reviews.link.edit');
+    Route::get('/admin/reviews/link/{report?}', 'Admin\ReviewLinkController@showList')->name('admin.reviews.link.list');
 
 });
 
