6c5a32d2e85a95e3d33f94a77efa9d8b672167b1
Ben Barden <github@benbarden.com>
2017-12-27 18:53:45 +0000

    Admin quickstats

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Services\FeedItemReviewService;
+
 class IndexController extends \App\Http\Controllers\BaseController
 {
     public function show()
@@ -10,6 +12,20 @@ class IndexController extends \App\Http\Controllers\BaseController
 
         $bindings['TopTitle'] = 'Admin index';
         $bindings['PanelTitle'] = 'Admin index';
+
+        // Quick stats
+        $feedItemReviewService = resolve('Services\FeedItemReviewService');
+        /* @var FeedItemReviewService $feedItemReviewService */
+
+        $gamesWithoutDevOrPub = $this->serviceGame->getWithoutDevOrPub();
+        $gamesWithoutVideos = $this->serviceGame->getWithoutVideoUrl();
+        $gamesWithoutGenres = $this->serviceGame->getGamesWithoutGenres();
+        $unprocessedFeedItems = $feedItemReviewService->getUnprocessed();
+
+        $bindings['GamesWithoutDevOrPubCount'] = $gamesWithoutDevOrPub->count();
+        $bindings['GamesWithoutVideosCount'] = $gamesWithoutVideos->count();
+        $bindings['GamesWithoutGenresCount'] = $gamesWithoutGenres->count();
+        $bindings['UnprocessedFeedItemsCount'] = $unprocessedFeedItems->count();
 
         return view('admin.index', $bindings);
     }
