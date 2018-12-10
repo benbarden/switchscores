ac8b8721df180f474313fd9e67f853cc9a3a7f2c
Ben Barden <github@benbarden.com>
2018-12-10 22:19:11 +0000

    Updating homepage links

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,39 +2,32 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\NewsService;
-use App\Services\ReviewLinkService;
-use App\Services\GameReleaseDateService;
-use App\Services\TopRatedService;
-use Carbon\Carbon;
+use Illuminate\Routing\Controller as Controller;
 
-class WelcomeController extends BaseController
+use App\Services\ServiceContainer;
+
+class WelcomeController extends Controller
 {
     public function show()
     {
+        $serviceContainer = \Request::get('serviceContainer');
+        /* @var $serviceContainer ServiceContainer */
+
         $regionCode = \Request::get('regionCode');
 
         $bindings = [];
 
-        $serviceReviewLinks = resolve('Services\ReviewLinkService');
-        /* @var $serviceReviewLinks ReviewLinkService */
-        $serviceGameReleaseDate = resolve('Services\GameReleaseDateService');
-        /* @var $serviceGameReleaseDate GameReleaseDateService */
-        $serviceTopRated = resolve('Services\TopRatedService');
-        /* @var $serviceTopRated TopRatedService */
+        $serviceReviewLinks = $serviceContainer->getReviewLinkService();
+        $serviceGameReleaseDate = $serviceContainer->getGameReleaseDateService();
+        $serviceTopRated = $serviceContainer->getTopRatedService();
 
         $bindings['ReviewList'] = $serviceReviewLinks->getLatestNaturalOrder(20);
         $bindings['NewReleases'] = $serviceGameReleaseDate->getReleased($regionCode, 20);
         $bindings['TopRatedAllTime'] = $serviceTopRated->getList($regionCode, 20);
 
-        // Charts
-        $chartsDateService = resolve('Services\ChartsDateService');
-        $bindings['ChartsLatestEu'] = $chartsDateService->getDateList('eu', 1);
-        $bindings['ChartsLatestUs'] = $chartsDateService->getDateList('us', 1);
-
         // Quick stats
         $bindings['TotalReleasedGames'] = $serviceGameReleaseDate->countReleased($regionCode);
-        $bindings['TotalUpcomingGames'] = $serviceGameReleaseDate->countUpcoming($regionCode);
+        $bindings['TotalRanked'] = $serviceTopRated->getCount($regionCode);
         $bindings['TotalReviews'] = $serviceReviewLinks->countActive();
 
         $bindings['TopTitle'] = 'Welcome';
