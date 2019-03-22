43a9d79ccba956b2c821b5d3d5474197fe766359
Ben Barden <628824+benbarden@users.noreply.github.com>
2019-03-22 09:23:08 +0000

    Expanding unit tests

--- tests/Unit/Construction/Game/GameBuilderTest.php
+++ tests/Unit/Construction/Game/GameBuilderTest.php
@@ -16,4 +16,225 @@ class GameBuilderTest extends TestCase
         $game = $gameBuilder->setTitle($title)->getGame();
         $this->assertEquals($title, $game->title);
     }
+
+    public function testLinkTitle()
+    {
+        $title = 'Yoshi';
+        $linkTitle = 'yoshi-game-title';
+
+        $gameBuilder = new GameBuilder();
+        $game = $gameBuilder
+            ->setTitle($title)
+            ->setLinkTitle($linkTitle)
+            ->getGame();
+        $this->assertEquals($linkTitle, $game->link_title);
+    }
+
+    public function testPriceEshop()
+    {
+        $title = 'Yoshi';
+        $priceEshop = '7.59';
+
+        $gameBuilder = new GameBuilder();
+        $game = $gameBuilder
+            ->setTitle($title)
+            ->setPriceEshop($priceEshop)
+            ->getGame();
+        $this->assertEquals($priceEshop, $game->price_eshop);
+    }
+
+    public function testPlayers()
+    {
+        $title = 'Yoshi';
+        $players = '2-4';
+
+        $gameBuilder = new GameBuilder();
+        $game = $gameBuilder
+            ->setTitle($title)
+            ->setPlayers($players)
+            ->getGame();
+        $this->assertEquals($players, $game->players);
+    }
+
+    public function testOverview()
+    {
+        $title = 'Yoshi';
+        $overview = 'A game featuring Yoshi that I generated for unit testing purposes only.';
+
+        $gameBuilder = new GameBuilder();
+        $game = $gameBuilder
+            ->setTitle($title)
+            ->setOverview($overview)
+            ->getGame();
+        $this->assertEquals($overview, $game->overview);
+    }
+
+    public function testDeveloper()
+    {
+        $title = 'Yoshi';
+        $developer = 'WOS Developer HQ';
+
+        $gameBuilder = new GameBuilder();
+        $game = $gameBuilder
+            ->setTitle($title)
+            ->setDeveloper($developer)
+            ->getGame();
+        $this->assertEquals($developer, $game->developer);
+    }
+
+    public function testPublisher()
+    {
+        $title = 'Yoshi';
+        $publisher = 'The Amazing Publishing House of WOS';
+
+        $gameBuilder = new GameBuilder();
+        $game = $gameBuilder
+            ->setTitle($title)
+            ->setPublisher($publisher)
+            ->getGame();
+        $this->assertEquals($publisher, $game->publisher);
+    }
+
+    public function testMediaFolder()
+    {
+        $title = 'Yoshi';
+        $mediaFolder = '/img/media/yoshi/';
+
+        $gameBuilder = new GameBuilder();
+        $game = $gameBuilder
+            ->setTitle($title)
+            ->setMediaFolder($mediaFolder)
+            ->getGame();
+        $this->assertEquals($mediaFolder, $game->media_folder);
+    }
+
+    public function testReviewCount()
+    {
+        $title = 'Yoshi';
+        $reviewCount = 10;
+
+        $gameBuilder = new GameBuilder();
+        $game = $gameBuilder
+            ->setTitle($title)
+            ->setReviewCount($reviewCount)
+            ->getGame();
+        $this->assertEquals($reviewCount, $game->review_count);
+    }
+
+    public function testAmazonUkLink()
+    {
+        $title = 'Yoshi';
+        $amazonUkLink = 'https://amazon.co.uk/blahblah';
+
+        $gameBuilder = new GameBuilder();
+        $game = $gameBuilder
+            ->setTitle($title)
+            ->setAmazonUkLink($amazonUkLink)
+            ->getGame();
+        $this->assertEquals($amazonUkLink, $game->amazon_uk_link);
+    }
+
+    public function testVideoUrl()
+    {
+        $title = 'Yoshi';
+        $videoUrl = 'https://youtube.com/blahblah';
+
+        $gameBuilder = new GameBuilder();
+        $game = $gameBuilder
+            ->setTitle($title)
+            ->setVideoUrl($videoUrl)
+            ->getGame();
+        $this->assertEquals($videoUrl, $game->video_url);
+    }
+
+    public function testBoxartSquareUrl()
+    {
+        $title = 'Yoshi';
+        $boxartSquareUrl = 'yoshi-boxart-square.jpg';
+
+        $gameBuilder = new GameBuilder();
+        $game = $gameBuilder
+            ->setTitle($title)
+            ->setBoxartSquareUrl($boxartSquareUrl)
+            ->getGame();
+        $this->assertEquals($boxartSquareUrl, $game->boxart_square_url);
+    }
+
+    public function testNintendoPageUrl()
+    {
+        $title = 'Yoshi';
+        $nintendoPageUrl = 'https://nintendo.co.uk/yoshi-commits-tax-fraud';
+
+        $gameBuilder = new GameBuilder();
+        $game = $gameBuilder
+            ->setTitle($title)
+            ->setNintendoPageUrl($nintendoPageUrl)
+            ->getGame();
+        $this->assertEquals($nintendoPageUrl, $game->nintendo_page_url);
+    }
+
+    public function testEshopEuropeFsId()
+    {
+        $title = 'Yoshi';
+        $fsId = 7575757;
+
+        $gameBuilder = new GameBuilder();
+        $game = $gameBuilder
+            ->setTitle($title)
+            ->setEshopEuropeFsId($fsId)
+            ->getGame();
+        $this->assertEquals($fsId, $game->eshop_europe_fs_id);
+    }
+
+    public function testBoxartHeaderImage()
+    {
+        $title = 'Yoshi';
+        $boxartHeaderImage = 'yoshi-boxart-header.jpg';
+
+        $gameBuilder = new GameBuilder();
+        $game = $gameBuilder
+            ->setTitle($title)
+            ->setBoxartHeaderImage($boxartHeaderImage)
+            ->getGame();
+        $this->assertEquals($boxartHeaderImage, $game->boxart_header_image);
+    }
+
+    public function testRatingAvg()
+    {
+        $title = 'Yoshi';
+        $ratingAvg = 7.5;
+
+        $gameBuilder = new GameBuilder();
+        $game = $gameBuilder
+            ->setTitle($title)
+            ->setRatingAvg($ratingAvg)
+            ->getGame();
+        $this->assertEquals($ratingAvg, $game->rating_avg);
+    }
+
+    public function testImageCount()
+    {
+        $title = 'Yoshi';
+        $imageCount = 2;
+
+        $gameBuilder = new GameBuilder();
+        $game = $gameBuilder
+            ->setTitle($title)
+            ->setImageCount($imageCount)
+            ->getGame();
+        $this->assertEquals($imageCount, $game->image_count);
+    }
+
+    public function testGameRank()
+    {
+        $title = 'Yoshi';
+        $gameRank = 129;
+
+        $gameBuilder = new GameBuilder();
+        $game = $gameBuilder
+            ->setTitle($title)
+            ->setGameRank($gameRank)
+            ->getGame();
+        $this->assertEquals($gameRank, $game->game_rank);
+    }
 }
