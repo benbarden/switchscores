62ae539a9655922e807b05997b5edfb70c84b397
Ben Barden <628824+benbarden@users.noreply.github.com>
2019-05-06 17:52:40 +0100

    Stop creating old-style user lists on registration

--- app/Listeners/CreateListsForNewUser.php
+++ app/Listeners/CreateListsForNewUser.php
@@ -3,8 +3,6 @@
 namespace App\Listeners;
 
 use App\Events\UserCreated;
-use App\UserList;
-use App\Services\UserListService;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
@@ -29,26 +27,5 @@ class CreateListsForNewUser
     public function handle(UserCreated $event)
     {
         $userId = $event->user->id;
-        $properties = [
-            'user_id' => $userId,
-        ];
-        $jsonProperties = json_encode($properties);
-
-        $userListService = resolve('Services\UserListService');
-        /* @var $userListService UserListService */
-
-        $userList = new UserList;
-        $userList->user_id = $userId;
-        $userList->list_type = UserList::LIST_TYPE_OWNED;
-        $userList->list_name = 'Owned list';
-        $userList->list_status = 1;
-        $userList->save();
-
-        $userList = new UserList;
-        $userList->user_id = $userId;
-        $userList->list_type = UserList::LIST_TYPE_WISHLIST;
-        $userList->list_name = 'Wishlist';
-        $userList->list_status = 1;
-        $userList->save();
     }
 }
