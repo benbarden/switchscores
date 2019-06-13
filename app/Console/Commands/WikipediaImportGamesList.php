729700dd3fa0bae2157212fa690da057e57811f6
Ben Barden <628824+benbarden@users.noreply.github.com>
2019-06-13 21:01:03 +0100

    Wikipedia importer: require either EU or US release date to be imported

--- app/Console/Commands/WikipediaImportGamesList.php
+++ app/Console/Commands/WikipediaImportGamesList.php
@@ -112,6 +112,13 @@ class WikipediaImportGamesList extends Command
                         ')');
                     */
                     continue;
+                } elseif ($countRealDates == 1) {
+                    // Check if we only have a Japanese release date
+                    if ($dateHandler->getUpcomingDate($feedItemGame->upcoming_date_jp) == null) {
+                        // This is the only record - skip for now
+                        $logger->error('Skipping as game only has a Japanese date');
+                        continue;
+                    }
                 }
                 
                 // See if we can locate the game
