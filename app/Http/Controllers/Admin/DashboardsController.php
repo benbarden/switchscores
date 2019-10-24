2e7ac4b00c7b3e44c0ef068d39f9d83543817e86
Ben Barden <628824+benbarden@users.noreply.github.com>
2019-10-24 21:47:15 +0100

    Moving admin dashboard content to staff index, updating links and redirecting /admin to /staff

--- app/Http/Controllers/Admin/DashboardsController.php
+++ app/Http/Controllers/Admin/DashboardsController.php
@@ -8,12 +8,6 @@ use App\Traits\WosServices;
 
 use App\Services\ServiceContainer;
 
-use App\SiteAlert;
-use App\ReviewUser;
-use App\PartnerReview;
-
-use App\Services\AdminDashboards\CategorisationService;
-
 class DashboardsController extends Controller
 {
     use WosServices;
@@ -33,43 +27,6 @@ class DashboardsController extends Controller
 
     public function index()
     {
-        $serviceContainer = \Request::get('serviceContainer');
-        /* @var $serviceContainer ServiceContainer */
-
-        $regionCode = \Request::get('regionCode');
-
-        $serviceUser = $serviceContainer->getUserService();
-
-        $feedItemGameService = $serviceContainer->getFeedItemGameService();
-        $serviceFeedItemReview = $serviceContainer->getFeedItemReviewService();
-
-        $serviceSiteAlert = $serviceContainer->getSiteAlertService();
-        $serviceEshopEurope = $serviceContainer->getEshopEuropeGameService();
-
-        $bindings = [];
-
-        $pageTitle = 'Admin dashboard';
-        $bindings['TopTitle'] = $pageTitle;
-        $bindings['PageTitle'] = $pageTitle;
-
-        // Approvals
-        $serviceMarioMakerLevels = $serviceContainer->getMarioMakerLevelService();
-        $bindings['MarioMakerLevelPendingCount'] = $serviceMarioMakerLevels->getPending()->count();
-
-        // Information and site stats
-        $bindings['RegisteredUserCount'] = $serviceUser->getCount();
-        $bindings['EshopEuropeUnlinkedCount'] = $serviceEshopEurope->getAllWithoutLink(null, true);
-
-        // Updates requiring approval
-        $unprocessedFeedReviewItems = $serviceFeedItemReview->getUnprocessed();
-        $pendingFeedGameItems = $feedItemGameService->getPending();
-        $bindings['UnprocessedFeedReviewItemsCount'] = count($unprocessedFeedReviewItems);
-        $bindings['PendingFeedGameItemsCount'] = count($pendingFeedGameItems);
-
-        // Action lists
-        $bindings['SiteAlertErrorCount'] = $serviceSiteAlert->countByType(SiteAlert::TYPE_ERROR);
-        $bindings['SiteAlertLatest'] = $serviceSiteAlert->getLatest(SiteAlert::TYPE_ERROR);
-
-        return view('admin.dashboards.index', $bindings);
+        return redirect('/staff');
     }
 }
--- app/Http/Controllers/Staff/IndexController.php
+++ app/Http/Controllers/Staff/IndexController.php
@@ -4,13 +4,49 @@ namespace App\Http\Controllers\Staff;
 
 use Illuminate\Routing\Controller as Controller;
 
+use App\Traits\SiteRequestData;
+use App\Traits\WosServices;
+
+use App\SiteAlert;
+
 class IndexController extends Controller
 {
+    use SiteRequestData;
+    use WosServices;
+
     public function index()
     {
+        $serviceUser = $this->getServiceUser();
+
+        $serviceFeedItemGame = $this->getServiceFeedItemGame();
+        $serviceFeedItemReview = $this->getServiceFeedItemReview();
+
+        $serviceSiteAlert = $this->getServiceSiteAlert();
+        $serviceEshopEurope = $this->getServiceEshopEuropeGame();
+
+        $bindings = [];
+
         $pageTitle = 'Staff index';
         $bindings['TopTitle'] = $pageTitle;
         $bindings['PageTitle'] = $pageTitle;
+
+        // Approvals
+        $serviceMarioMakerLevels = $this->getServiceMarioMakerLevel();
+        $bindings['MarioMakerLevelPendingCount'] = $serviceMarioMakerLevels->getPending()->count();
+
+        // Information and site stats
+        $bindings['RegisteredUserCount'] = $serviceUser->getCount();
+        $bindings['EshopEuropeUnlinkedCount'] = $serviceEshopEurope->getAllWithoutLink(null, true);
+
+        // Updates requiring approval
+        $unprocessedFeedReviewItems = $serviceFeedItemReview->getUnprocessed();
+        $pendingFeedGameItems = $serviceFeedItemGame->getPending();
+        $bindings['UnprocessedFeedReviewItemsCount'] = count($unprocessedFeedReviewItems);
+        $bindings['PendingFeedGameItemsCount'] = count($pendingFeedGameItems);
+
+        // Action lists
+        $bindings['SiteAlertErrorCount'] = $serviceSiteAlert->countByType(SiteAlert::TYPE_ERROR);
+        $bindings['SiteAlertLatest'] = $serviceSiteAlert->getLatest(SiteAlert::TYPE_ERROR);
 
         return view('staff.index', $bindings);
     }
