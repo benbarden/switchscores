4ad846c2ff41d1bbf14edb0cfce68ff3f6f723a5
Ben Barden <628824+benbarden@users.noreply.github.com>
2021-06-14 20:09:48 +0100

    Revamped games company dashboard

--- app/Http/Controllers/GamesCompanies/IndexController.php
+++ app/Http/Controllers/GamesCompanies/IndexController.php
@@ -18,6 +18,30 @@ class IndexController extends Controller
 
         $bindings['ReviewSitesWithContactDetails'] = $this->getServicePartner()->getActiveReviewSitesWithContactDetails();
 
+        $authUser = $this->getValidUser($this->getServiceUser());
+        $partnerId = $authUser->games_company_id;
+        $partnerData = $authUser->gamesCompany;
+
+        $gameDevList = $this->getServiceGameDeveloper()->getGamesByDeveloper($partnerId, false);
+        $gamePubList = $this->getServiceGamePublisher()->getGamesByPublisher($partnerId, false);
+
+        $mergedGameList = $this->getServicePartner()->getMergedGameList($gameDevList, $gamePubList);
+
+        $mergedGameList = collect($mergedGameList)->sortBy('eu_release_date')->reverse()->toArray();
+
+        $releasedGames = collect($mergedGameList)->where('eu_is_released', '=', '1')->toArray();
+        $upcomingGames = collect($mergedGameList)->where('eu_is_released', '=', '0')->toArray();
+
+        $rankedGames = collect($releasedGames)->where('review_count', '>=', '3')->toArray();
+        $unrankedGames = collect($releasedGames)->where('review_count', '<', '3')->toArray();
+
+        $bindings['MergedGameList'] = $mergedGameList;
+        $bindings['ReleasedGames'] = $releasedGames;
+        $bindings['UpcomingGames'] = $upcomingGames;
+        $bindings['RankedGames'] = $rankedGames;
+        $bindings['UnrankedGames'] = $unrankedGames;
+        $bindings['PartnerData'] = $partnerData;
+
         $pageTitle = 'Games company dashboard';
         $bindings['TopTitle'] = $pageTitle;
         $bindings['PageTitle'] = $pageTitle;
