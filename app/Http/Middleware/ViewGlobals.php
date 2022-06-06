31401905a31a1bd63b2d4b540955086103bc5143
Ben Barden <628824+benbarden@users.noreply.github.com>
2022-06-06 08:10:07 +0100

    Dynamic copyright

--- app/Http/Middleware/ViewGlobals.php
+++ app/Http/Middleware/ViewGlobals.php
@@ -9,6 +9,7 @@ class ViewGlobals
     public function handle($request, Closure $next)
     {
         \View::share('siteEnv', env('APP_ENV'));
+        \View::share('thisYear', date('Y'));
 
         return $next($request);
     }
