79e1e5d71e752dc1ccdfc743d86e481994251134
Ben Barden <628824+benbarden@users.noreply.github.com>
2022-10-21 16:40:31 +0100

    DownloadImagesByScraper: increase limit to 100 items

--- app/Console/Commands/DataSource/NintendoCoUk/DownloadImagesByScraper.php
+++ app/Console/Commands/DataSource/NintendoCoUk/DownloadImagesByScraper.php
@@ -54,7 +54,7 @@ class DownloadImagesByScraper extends Command
         $logger->info('Loading data...');
 
         $repoGameLists = new RepoGameLists();
-        $gameList = $repoGameLists->noNintendoCoUkIdWithStoreOverride(10);
+        $gameList = $repoGameLists->noNintendoCoUkIdWithStoreOverride(100);
 
         $logger->info('Found '.count($gameList).' item(s); processing');
 
