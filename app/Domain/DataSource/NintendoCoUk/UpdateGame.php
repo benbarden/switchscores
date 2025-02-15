0696cb231cd66de6ad4f79a76a488aa82bbb4fde
Ben Barden <628824+benbarden@users.noreply.github.com>
2025-02-15 20:18:47 +0000

    Changing to use Eloquent not DB

--- app/Domain/DataSource/NintendoCoUk/UpdateGame.php
+++ app/Domain/DataSource/NintendoCoUk/UpdateGame.php
@@ -4,41 +4,30 @@ namespace App\Domain\DataSource\NintendoCoUk;
 
 use App\Models\Game;
 use App\Models\DataSource;
-
-use Illuminate\Support\Facades\DB;
+use App\Models\DataSourceParsed;
 
 class UpdateGame
 {
     public function updateDigitalAvailable()
     {
-        $sourceId = DataSource::DSID_NINTENDO_CO_UK;
-        DB::update('
-            UPDATE games
-            SET format_digital = ?
-            WHERE eshop_europe_fs_id IN (
-                SELECT link_id FROM data_source_parsed WHERE source_id = ?
-            );
-        ', [Game::FORMAT_AVAILABLE, $sourceId]);
+        $linkIds = DataSourceParsed::where('source_id', DataSource::DSID_NINTENDO_CO_UK)->get()->pluck('link_id');
 
-        DB::update('
-            UPDATE games
-            SET format_digital = ?
-            WHERE nintendo_store_url_override IS NOT NULL
-            AND format_digital = ?
-        ', [Game::FORMAT_AVAILABLE, Game::FORMAT_DELISTED]);
+        Game::whereIn('eshop_europe_fs_id', $linkIds)
+            ->update(['format_digital' => Game::FORMAT_AVAILABLE]);
+
+        Game::whereNotNull('nintendo_store_url_override')
+            ->where('format_digital', Game::FORMAT_DELISTED)
+            ->update(['format_digital' => Game::FORMAT_AVAILABLE]);
+
     }
 
     public function updateDigitalDelisted()
     {
-        $sourceId = DataSource::DSID_NINTENDO_CO_UK;
-        DB::update('
-            UPDATE games
-            SET format_digital = ?
-            WHERE nintendo_store_url_override IS NULL
-            AND eshop_europe_fs_id NOT IN (
-                SELECT link_id FROM data_source_parsed WHERE source_id = ?
-            );
-        ', [Game::FORMAT_DELISTED, $sourceId]);
+        $linkIds = DataSourceParsed::where('source_id', DataSource::DSID_NINTENDO_CO_UK)->get()->pluck('link_id');
+
+        Game::whereNull('nintendo_store_url_override')
+            ->whereNotIn('eshop_europe_fs_id', $linkIds)
+            ->update(['format_digital' => Game::FORMAT_DELISTED]);
     }
 
 }
