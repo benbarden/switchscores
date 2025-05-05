692b1f3496906fce1aa373429fc4e36d8c506663
Ben Barden <628824+benbarden@users.noreply.github.com>
2025-05-05 21:28:43 +0100

    Updating redirects to 301 (default is 302)

--- routes/public.php
+++ routes/public.php
@@ -31,9 +31,9 @@ Route::get('/help', 'PublicSite\HelpController@landing')->name('help.landing');
 Route::get('/help/low-quality-filter', 'PublicSite\HelpController@lowQualityFilter')->name('help.low-quality-filter');
 
 // Lists
-Route::redirect('/lists', '/games')->name('lists.landing');
-Route::redirect('/games/recent', '/c/switch-1/new-releases')->name('games.recentReleases');
-Route::redirect('/games/upcoming', '/c/switch-1/upcoming')->name('games.upcomingReleases');
+Route::redirect('/lists', '/c/switch-1', 301)->name('lists.landing');
+Route::redirect('/games/recent', '/c/switch-1/new-releases', 301)->name('games.recentReleases');
+Route::redirect('/games/upcoming', '/c/switch-1/upcoming', 301)->name('games.upcomingReleases');
 Route::get('/games/on-sale', 'PublicSite\ListsController@gamesOnSale')->name('games.onSale');
 Route::get('/reviews', 'PublicSite\ListsController@recentReviews')->name('reviews.landing');
 Route::get('/lists/recently-ranked', 'PublicSite\ListsController@recentlyRanked')->name('lists.recently-ranked');
@@ -74,29 +74,29 @@ Route::controller('PublicSite\Console\BrowseByDateController')->group(function (
 });
 
 // Main game pages
-Route::redirect('/games', '/c/switch-1')->name('games.landing');
+Route::redirect('/games', '/c/switch-1', 301)->name('games.landing');
 Route::match(['get', 'post'], '/games/search', 'PublicSite\Games\SearchController@show')->name('games.search');
 
 // Browse by...
-Route::redirect('/games/by-title', '/c/switch-1')->name('games.browse.byTitle.landing');
-Route::redirect('/games/by-title/{letter}', '/c/switch-1')->name('games.browse.byTitle.page');
+Route::redirect('/games/by-title', '/c/switch-1', 301)->name('games.browse.byTitle.landing');
+Route::redirect('/games/by-title/{letter}', '/c/switch-1', 301)->name('games.browse.byTitle.page');
 
-Route::redirect('/games/by-category', '/c/switch-1/category')->name('games.browse.byCategory.landing');
-Route::redirect('/games/by-category/{category}', '/c/switch-1/category/{category}')->name('games.browse.byCategory.page');
+Route::redirect('/games/by-category', '/c/switch-1/category', 301)->name('games.browse.byCategory.landing');
+Route::redirect('/games/by-category/{category}', '/c/switch-1/category/{category}', 301)->name('games.browse.byCategory.page');
 
-Route::redirect('/games/by-series', '/c/switch-1/series')->name('games.browse.bySeries.landing');
-Route::redirect('/games/by-series/{series}', '/c/switch-1/series/{series}')->name('games.browse.bySeries.page');
+Route::redirect('/games/by-series', '/c/switch-1/series', 301)->name('games.browse.bySeries.landing');
+Route::redirect('/games/by-series/{series}', '/c/switch-1/series/{series}', 301)->name('games.browse.bySeries.page');
 
-Route::redirect('/games/by-collection', '/c/switch-1/collection')->name('games.browse.byCollection.landing');
-Route::redirect('/games/by-collection/{collection}', '/c/switch-1/collection/{collection}')->name('games.browse.byCollection.page');
+Route::redirect('/games/by-collection', '/c/switch-1/collection', 301)->name('games.browse.byCollection.landing');
+Route::redirect('/games/by-collection/{collection}', '/c/switch-1/collection/{collection}', 301)->name('games.browse.byCollection.page');
 //Route::get('/games/by-collection/{collection}/category/{category}', 'PublicSite\Games\BrowseByCollectionController@pageCategory')->name('games.browse.byCollection.pageCategory');
 //Route::get('/games/by-collection/{collection}/series/{series}', 'PublicSite\Games\BrowseByCollectionController@pageSeries')->name('games.browse.byCollection.pageSeries');
 
-Route::redirect('/games/by-tag', '/c/switch-1/tag')->name('games.browse.byTag.landing');
-Route::redirect('/games/by-tag/{tag}', '/c/switch-1/tag/{tag}')->name('games.browse.byTag.page');
+Route::redirect('/games/by-tag', '/c/switch-1/tag', 301)->name('games.browse.byTag.landing');
+Route::redirect('/games/by-tag/{tag}', '/c/switch-1/tag/{tag}', 301)->name('games.browse.byTag.page');
 
-Route::redirect('/games/by-date', '/c/switch-1/2025')->name('games.browse.byDate.landing');
-Route::redirect('/games/by-date/{date}', '/c/switch-1/2025')->name('games.browse.byDate.page');
+Route::redirect('/games/by-date', '/c/switch-1/2025', 301)->name('games.browse.byDate.landing');
+Route::redirect('/games/by-date/{date}', '/c/switch-1/2025', 301)->name('games.browse.byDate.page');
 
 // Random
 Route::get('/games/random', 'PublicSite\Games\RandomController@getRandom')->name('game.random');
@@ -106,10 +106,10 @@ Route::get('/games/{id}', 'PublicSite\Games\GameShowController@showId')->name('g
 Route::get('/games/{id}/{linkTitle}', 'PublicSite\Games\GameShowController@show')->name('game.show');
 
 /* Top Rated */
-Route::redirect('/top-rated', '/c/switch-1/top-rated')->name('topRated.landing');
-Route::redirect('/top-rated/all-time', '/c/switch-1/top-rated/all-time')->name('topRated.allTime');
-Route::redirect('/top-rated/all-time/page/{page}', '/c/switch-1/top-rated/all-time/page/{page}')->name('topRated.allTime.page');
-Route::redirect('/top-rated/by-year/{year}', '/c/switch-1/top-rated/by-year/{year}')->name('topRated.byYear');
+Route::redirect('/top-rated', '/c/switch-1/top-rated', 301)->name('topRated.landing');
+Route::redirect('/top-rated/all-time', '/c/switch-1/top-rated/all-time', 301)->name('topRated.allTime');
+Route::redirect('/top-rated/all-time/page/{page}', '/c/switch-1/top-rated/all-time/page/{page}', 301)->name('topRated.allTime.page');
+Route::redirect('/top-rated/by-year/{year}', '/c/switch-1/top-rated/by-year/{year}', 301)->name('topRated.byYear');
 
 /* Reviews */
 Route::redirect('/reviews/{year}', '/c/switch-1/{year}')->name('reviews.landing.byYear');
@@ -138,12 +138,12 @@ Route::get('/community', 'PublicSite\CommunityController@landing')->name('commun
 
 // Sitemaps
 Route::controller('SitemapController')->group(function () {
-    Route::redirect('/sitemap', '/sitemaps/index.xml');
-    Route::redirect('/sitemap/site', '/sitemaps/sitemap-site.xml');
-    Route::redirect('/sitemap/games', '/sitemaps/sitemap-games.xml');
-    Route::redirect('/sitemap/calendar', '/sitemaps/sitemap-calendar.xml');
-    Route::redirect('/sitemap/top-rated', '/sitemaps/sitemap-top-rated.xml');
-    Route::redirect('/sitemap/reviews', '/sitemaps/sitemap-reviews.xml');
-    Route::redirect('/sitemap/tags', '/sitemaps/sitemap-tags.xml');
-//    Route::redirect('/sitemap/news', '/sitemaps/sitemap-news.xml');
+    Route::redirect('/sitemap', '/sitemaps/index.xml', 301);
+    Route::redirect('/sitemap/site', '/sitemaps/sitemap-site.xml', 301);
+    Route::redirect('/sitemap/games', '/sitemaps/sitemap-games.xml', 301);
+    Route::redirect('/sitemap/calendar', '/sitemaps/sitemap-calendar.xml', 301);
+    Route::redirect('/sitemap/top-rated', '/sitemaps/sitemap-top-rated.xml', 301);
+    Route::redirect('/sitemap/reviews', '/sitemaps/sitemap-reviews.xml', 301);
+    Route::redirect('/sitemap/tags', '/sitemaps/sitemap-tags.xml', 301);
+//    Route::redirect('/sitemap/news', '/sitemaps/sitemap-news.xml', 301);
 });
