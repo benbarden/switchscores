7ce287a5bdd37fd572debd16a15e548565df08be
Ben Barden <628824+benbarden@users.noreply.github.com>
2025-09-07 09:41:14 +0100

    Increase cache duration for certain pages; extending stale-while-revalidate for all

--- app/Http/Middleware/CacheAnonymousPages.php
+++ app/Http/Middleware/CacheAnonymousPages.php
@@ -15,11 +15,20 @@ class CacheAnonymousPages
             /*
              * s-maxage=3600: CF edge caching for 1 hour
              * max-age=300: Browser caching for 5 minutes
+             * stale-while-revalidate=600: CF serves stale content for 10 mins even when cache expires
              */
-            $response->headers->set(
-                'Cache-Control',
-                'public, s-maxage=3600, max-age=300, stale-while-revalidate=120, stale-if-error=600'
-            );
+            if (preg_match('#^/(games/|partners/games-company/)#', $request->getPathInfo())) {
+                // Extend cache to 24 hours for large groups of pages
+                $response->headers->set(
+                    'Cache-Control',
+                    'public, s-maxage=86400, max-age=300, stale-while-revalidate=600, stale-if-error=600'
+                );
+            } else {
+                $response->headers->set(
+                    'Cache-Control',
+                    'public, s-maxage=3600, max-age=300, stale-while-revalidate=600, stale-if-error=600'
+                );
+            }
 
             // Strip cookies if no session cookie in request
             if (!$request->hasCookie('laravel_session')) {
