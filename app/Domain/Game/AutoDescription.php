b6a91b19688e43aa9f6ddf67af9edfd299816960
Ben Barden <628824+benbarden@users.noreply.github.com>
2025-12-26 10:06:48 +0000

    Wording tweaks for unranked game blurb

--- app/Domain/Game/AutoDescription.php
+++ app/Domain/Game/AutoDescription.php
@@ -102,13 +102,13 @@ class AutoDescription
         if (!$this->game->isDigitalDelisted() && (!$this->game->game_rank) && ($this->game->eu_is_released == 1) && ($this->game->is_low_quality == 0)) {
             switch ($this->game->review_count) {
                 case 0:
-                    $blurb = 'As it has no reviews, it is currently unranked. We need 3 reviews to give the game a rank. ';
+                    $blurb = 'This game hasn\'t been ranked yet, as it doesn\'t have any reviews. We need 3 reviews to give it a rank. ';
                     break;
                 case 1:
-                    $blurb = 'As it only has 1 review, it is currently unranked. We need 2 more reviews to give the game a rank. ';
+                    $blurb = 'This game hasn\'t been ranked yet, as it only has 1 review. We need 2 more reviews to give it a rank. ';
                     break;
                 case 2:
-                    $blurb = 'As it only has 2 reviews, it is currently unranked. We need 1 more review to give the game a rank. ';
+                    $blurb = 'This game hasn\'t been ranked yet, as it only has 2 reviews. We need 1 more review to give it a rank. ';
                     break;
                 default:
                     break;
